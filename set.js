03be0645b80e513483c9c79ee24fa62c7f0a0933
kaungyelu <kaung2016.ye@gmail.com>
2025-12-10 15:47:03 +0630

    Update set.js

--- set.js
+++ set.js
@@ -1,6 +1,6 @@
         // Supabase Configuration
-        const SUPABASE_URL = 'https://ugfmzaxrbxvjecqpezdm.supabase.co';
-        const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVnZm16YXhyYnh2amVjcXBlemRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ5OTc0NDMsImV4cCI6MjA4MDU3MzQ0M30.rEjWWdKVc_oikOFzQeBJ3I1j3Xke4Pla5DMbsQtugjU';
+        const SUPABASE_URL = 'process.env.SUPABASE_URL';
+        const SUPABASE_ANON_KEY = 'process.env.SUPABASE_ANON_KEY';
         
         // Initialize Supabase client
         let supabase;
