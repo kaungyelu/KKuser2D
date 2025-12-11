5a1175cc7001905175b34625d1a385a78a7415f7
kaungyelu <kaung2016.ye@gmail.com>
2025-12-11 17:08:34 +0630

    Update name.js

--- name.js
+++ name.js
@@ -1,7 +1,4 @@
     
-        // Supabase Configuration
-        const SUPABASE_URL = 'https://ugfmzaxrbxvjecqpezdm.supabase.co';
-        const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVnZm16YXhyYnh2amVjcXBlemRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ5OTc0NDMsImV4cCI6MjA4MDU3MzQ0M30.rEjWWdKVc_oikOFzQeBJ3I1j3Xke4Pla5DMbsQtugjU';
         
         // Initialize Supabase client
         let supabase;
